b71d040220fbf9a98ad55693a2cfbd4f39b8743c
yash <yash2301>
2021-07-23 18:13:03 +0530

    update form

--- src/components/ScheduleDialoge.jsx
+++ src/components/ScheduleDialoge.jsx
@@ -4,14 +4,11 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const useStyles = makeStyles((theme) => ({
     form: {
@@ -36,7 +33,25 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ScheduleDialoge(props) {
     const classes = useStyles();
-    
+
+    const [ scheduleForm, setScheduleForm ] = useState({})
+
+    const onValueChange = (event)=>{
+        const { name, value } = event.target;
+        setScheduleForm({
+            ...scheduleForm,
+            [name]: value
+        })
+    }
+
+    useEffect(()=>{
+        setScheduleForm({
+            meetingTime:'',
+            title:'',
+            description:'',
+        })
+    },[]);
+
     return (
         <Dialog
             fullWidth={"md"}
@@ -46,7 +61,7 @@ export default function ScheduleDialoge(props) {
             aria-labelledby="max-width-dialog-title"
         >
             <DialogTitle id="max-width-dialog-title">Add Schedule</DialogTitle>
-            <DialogContent>
+            <DialogContent >
                 <form noValidate>
                     <div>
                         <FormControl className={classes.formControl}>
@@ -57,8 +72,10 @@ export default function ScheduleDialoge(props) {
                                 defaultValue="2017-05-24T10:30"
                                 className={classes.textField}
                                 InputLabelProps={{
-                                shrink: true,
+                                    shrink: true,
                                 }}
+                                name="meetingTime"
+                                onChange={onValueChange}
                             />
                         </FormControl>
                     </div>
@@ -70,8 +87,10 @@ export default function ScheduleDialoge(props) {
                                 type="text"
                                 className={classes.textField}
                                 InputLabelProps={{
-                                shrink: true,
+                                    shrink: true,
                                 }}
+                                name="title"
+                                onChange={onValueChange}
                             />
                         </FormControl>
                     </div>
@@ -84,12 +103,19 @@ export default function ScheduleDialoge(props) {
                                 rows={2}
                                 multiline
                                 rowsMax={6}
+                                name="description"
+                                onChange={onValueChange}
                             />
                         </FormControl>
                     </div>
                 </form>
                 <div className={classes.buttonGroup} >
-                    <Button onClick={props.saveDialog} className={classes.buttonScd} variant="contained" color="primary">
+                    <Button onClick={()=>{
+                        console.log(scheduleForm);
+                        if(scheduleForm.meetingTime != '' && scheduleForm.title != '' && scheduleForm.description != ''){
+                            props.saveDialog(scheduleForm)
+                        }
+                    }} className={classes.buttonScd} variant="contained" color="primary">
                         Save
                     </Button>
                     <Button onClick={props.closeDialog} className={classes.buttonScd}  variant="contained" color="primary">
